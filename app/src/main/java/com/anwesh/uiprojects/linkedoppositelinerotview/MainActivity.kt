309d4715ff1de685e1f770066bd9c5fe3e2cf4fa
Anwesh43 <anweshthecool0@gmail.com>
2019-07-09 10:37:14 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedoppositelinerotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedoppositelinerotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedoppositelinerotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.oppositelinerotview.OppositeLineRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        OppositeLineRotView.create(this)
     }
 }
