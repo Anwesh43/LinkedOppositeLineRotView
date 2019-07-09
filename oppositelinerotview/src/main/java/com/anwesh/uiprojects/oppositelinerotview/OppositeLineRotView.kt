e326def249d1b1d160dc35d3980e3bd856afc96b
Anwesh43 <anweshthecool0@gmail.com>
2019-07-09 10:19:30 +0530

    adding OppositeLineRotView class

--- oppositelinerotview/src/main/java/com/anwesh/uiprojects/oppositelinerotview/OppositeLineRotView.kt
+++ oppositelinerotview/src/main/java/com/anwesh/uiprojects/oppositelinerotview/OppositeLineRotView.kt
@@ -60,3 +60,21 @@ fun Canvas.drawOLRNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class OppositeLineRotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+} 
